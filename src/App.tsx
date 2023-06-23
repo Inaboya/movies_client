b085bfa6b57973b806ff7e9e81d98be88d693da8
Inaboya <momohdavid21@gmail.com>
2023-06-23 09:27:02 +0100

    create redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  // useEffect(() => {
+  //   if (localStorage.token) {
+  //     setAuthToken(localStorage.token);
+  //   }
+
+  //   store.dispatch(loadUser());
+
+  //   // Log user out from all tabs if they log out in one tab
+
+  //   window.addEventListener('storage', () => {
+  //     if (!localStorage.token) store.dispatch({ type: LOGOUT });
+  //   })
+  // }, [])
   return (
     <>
       <h1>Movie app</h1>
