2b715744133551337574eb645d63a7ee591d1d18
Inaboya <momohdavid21@gmail.com>
2023-06-23 10:45:56 +0100

    implement auth reducer

--- src/reducer/auth.ts
+++ src/reducer/auth.ts
@@ -1,1 +1,77 @@
 import { ActionTypes } from "../actions/actionTypes";
+import { Action } from "../actions/actions";
+import { AuthInitialState } from "../utils/typings";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: false,
+  loading: true,
+  user: null,
+  error: null,
+} as AuthInitialState;
+
+const authReducer = (state = initialState, action: Action) => {
+  switch (action.type) {
+    case ActionTypes.REGISTER_USER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: false,
+        loading: false,
+      };
+
+    case ActionTypes.REGISTER_USER_FAIL:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
+
+    case ActionTypes.LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+
+    case ActionTypes.LOGIN_USER_FAIL:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
+
+    case ActionTypes.LOAD_USER_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
+
+    case ActionTypes.LOAD_USER_FAIL:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+      };
+
+    case ActionTypes.LOGOUT_USER:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+      };
+  }
+};
--- src/utils/typings.ts
+++ src/utils/typings.ts
@@ -1,17 +1,18 @@
-interface UserPayload {
+export interface UserPayload {
   name: string;
   email: string;
   password: string;
   password2: string;
 }
 
-interface AuthInitialState {
+export interface AuthInitialState {
   token: string | null;
   isAuthenticated: boolean;
   loading: boolean;
   user: UserPayload | null;
+  error: string | null;
 }
 
-interface MovieInitialState {
+export interface MovieInitialState {
   movies: any[];
 }
