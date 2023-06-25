33dc3f90d2cdb73fce7c3c3adc42323c12a59390
Inaboya <56197975+Inaboya@users.noreply.github.com>
2023-06-25 17:30:35 +0100

    Update FavoriteMovie.tsx

--- src/components/FavoriteMovie.tsx
+++ src/components/FavoriteMovie.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import MovieCard from "./MovieCard";
 import FavoriteMovieCard from "./FavoriteMovieCard";
 
 interface Props {
@@ -6,13 +7,13 @@ interface Props {
 }
 
 const FavoriteMovies: React.FC<Props> = ({ movies }) => {
-  // console.log(movies, "movies from movie component");
+  console.log(movies, "movies from movie component");
   // const
   return (
     <div className="movie-container">
       {movies &&
         movies.map((movie: any) => (
-          <FavoriteMovieCard key={movie._id} movie={movie.movieId} />
+          <FavoriteMovieCard key={movie._id} movie={movie} />
         ))}
     </div>
   );
