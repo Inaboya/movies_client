e4963b1bc0a3d05753a32fdd2366e7db79148230
Inaboya <56197975+Inaboya@users.noreply.github.com>
2023-06-25 15:33:38 +0100

    Update FavoriteMovieCard.tsx

--- src/components/FavoriteMovieCard.tsx
+++ src/components/FavoriteMovieCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import "./components.css";
 import { Link } from "react-router-dom";
 import "./components.css";
@@ -9,33 +9,29 @@ interface Props {
 }
 
 const FavoriteMovieCard: React.FC<Props> = ({ movie }) => {
-    console.log(movie, "movie from favorite movie card")
+    console.log(movie, "movie card")
   return (
     <div className="movie-card">
       <div className="image-container">
         <img
-          src={`https://image.tmdb.org/t/p/w500${movie?.movieId?.poster_path}`}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie?.title}
           className="movie-card-image"
         />
       </div>
 
       <div className="card-content">
-        <h2 className="card-content-title">{movie.movie?.movieId?.title}</h2>
-        <p className="card-content-title">{movie.movie?.movieId?.release_date}</p>
+        <h2 className="card-content-title">{movie?.title}</h2>
+        <p className="card-content-title">{movie?.release_date}</p>
 
         <div className="card-footer">
-          <Link to={`/favorite-movies/${movie?.movieId?._id}`} className="card-footer-link">
+          <Link to={`/favorite-movies/${movie?._id}`} className="card-footer-link">
             View Details
           </Link>
         </div>
       </div>
     </div>
   );
-};
-
-FavoriteMovieCard.propTypes = {
-  movie: PropTypes.object.isRequired,
 };
 
 export default FavoriteMovieCard;
